84996de6e6fed6192227746a4d05b8b324b7f5d4
Ben Wilker <ben.wilker@musco.com>
2025-08-22 11:35:49 -0500

    Update main.py
    update

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """Takes a filepath and returns the contents of the file as a string."""
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    # Use the relative path to your frankenstein.txt file
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+# Call main to run program
+if __name__ == "__main__":
+    main()
